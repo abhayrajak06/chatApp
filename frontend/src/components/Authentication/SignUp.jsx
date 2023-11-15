cb343600fdb663cc98a01848c9bb9bffb83f954b
Abhay Rajak <mail2rajab01@gmail.com>
2023-11-15 23:57:00 +0530

    making signup page

--- frontend/src/components/Authentication/SignUp.jsx
+++ frontend/src/components/Authentication/SignUp.jsx
@@ -1,12 +1,34 @@
 import { FormControl, FormLabel, Input, VStack } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 
 const SignUp = () => {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [confirmpassword, setConfirmpassword] = useState();
+  const [pic, setPic] = useState();
   return (
     <VStack>
       <FormControl isRequired>
         <FormLabel>Name</FormLabel>
-        <Input placeholder="Enter Your Name" />
+        <Input
+          placeholder="Enter Your Name"
+          onChange={(e) => setName(e.target.value)}
+        />
+      </FormControl>
+      <FormControl isRequired>
+        <FormLabel>Email</FormLabel>
+        <Input
+          placeholder="Enter Your Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormControl>
+      <FormControl isRequired>
+        <FormLabel>Password</FormLabel>
+        <Input
+          placeholder="Enter Your Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </FormControl>
     </VStack>
   );
