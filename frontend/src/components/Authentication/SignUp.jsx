39fbf18aea28ba4f6052ae3b5b25b01707a5f140
Abhay Rajak <mail2rajab01@gmail.com>
2023-11-21 23:59:32 +0530

    test

--- frontend/src/components/Authentication/SignUp.jsx
+++ frontend/src/components/Authentication/SignUp.jsx
@@ -6,20 +6,66 @@ import {
   InputGroup,
   InputRightElement,
   VStack,
+  useToast,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
+import axios from "axios";
 
 const SignUp = () => {
-  const [name, setName] = useState();
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
-  const [confirmpassword, setConfirmpassword] = useState();
-  const [pic, setPic] = useState();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [pic, setPic] = useState("");
   const [show, setShow] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const toast = useToast();
 
   const handleClick = () => setShow(!show);
 
-  const postDetails = (pics) => {};
+  const postDetails = async (pics) => {
+    setLoading(true);
+    if (!pics) {
+      toast({
+        title: "Please Select an Image!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+      });
+      setLoading(false);
+      return;
+    }
+
+    if (pics.type === "image/jpeg" || pics.type === "image/png") {
+      const preset = import.meta.env.VITE_REACT_APP_CLOUD_NAME;
+      const cloudName = import.meta.env.VITE_REACT_APP_PRESET_NAME;
+
+      try {
+        const formData = new FormData();
+        formData.append("file", pics);
+        formData.append("upload_preset", preset);
+
+        const response = await axios.post(
+          `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
+          formData
+        );
+        setLoading(false);
+        setPic(response?.data?.url);
+        console.log("Image uploaded successfully:", response?.data?.url);
+      } catch (error) {
+        console.error("Error uploading image:", error);
+        setLoading(false);
+      }
+    } else {
+      toast({
+        title: "Please Select a valid Image (JPEG/PNG)",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+      });
+      setLoading(false);
+    }
+  };
 
   const submitHandler = () => {};
 
@@ -60,7 +106,7 @@ const SignUp = () => {
           <Input
             type={show ? "text" : "password"}
             placeholder="confirm password"
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => setConfirmPassword(e.target.value)}
           />
           <InputRightElement width={"4.5rem"}>
             <Button h={"1.8rem"} size={"sm"} onClick={handleClick}>
@@ -84,6 +130,7 @@ const SignUp = () => {
         width={"100%"}
         style={{ marginTop: 15 }}
         onClick={submitHandler}
+        isLoading={loading}
       >
         Sign Up
       </Button>
